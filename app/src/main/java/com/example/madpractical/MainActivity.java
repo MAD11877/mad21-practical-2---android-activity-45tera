862abdad176ea2cb6926a542660847084727291c
s10221930@connect.np.edu.sg <s10221930@connect.np.edu.sg>
2022-04-28 23:57:45 +0800

    Secondary Commit, completed the challenge successfully.

--- app/src/main/java/com/example/madpractical/MainActivity.java
+++ app/src/main/java/com/example/madpractical/MainActivity.java
@@ -5,26 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    //New user on start
-    User user1 = new User("guest","guest",1,false);
-    //button change
-    Button but = (Button)findViewById(R.id.follow);
+    Button but;
+    User user1 = new User("guest","guest",1,true);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+       super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //New user on start
+        but = findViewById(R.id.follow);
+
+        //button change
+        // but.setOnClickListener((View.OnClickListener) this);
+        //but.setOnClickListener(new View.OnClickListener()){
+        // public void onClick(View v){
+        // log.v()
+        // }
+        // }
+
         if (!user1.followed){
             but.setText("Follow");
         }
         else{
             but.setText("Followed");
         }
+
     }
     public void onClick(View v){
-
         if(but.getText().toString().equals("Followed")){ //unfollowed
             but.setText("Follow");
             user1.followed= false;
